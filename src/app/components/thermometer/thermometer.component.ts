b6f0ce0a4d4152dd6dd90773fa88ffbe6251016b
feldmant <feldmant@mit.edu>
2019-05-01 19:51:07 -0400

    thermometer working

--- src/app/components/thermometer/thermometer.component.ts
+++ src/app/components/thermometer/thermometer.component.ts
@@ -11,10 +11,14 @@ import { TemperatureService } from '../../services/temperature-service.service';
 export class ThermometerComponent implements OnInit {
 	temperature: number;
 	_subscription: any;
+	thermHeight: number = 130;
 
 	constructor(private provider: TemperatureService) {
 		this.temperature = this.provider.temperature;
-		this._subscription = this.provider.tempChange.subscribe((value) => { this.temperature = value })
+		this._subscription = this.provider.tempChange.subscribe((value) => { 
+			this.temperature = value; 
+			this.calcHeight(); })
+		this.calcHeight();
 	}
 	
 	ngOnInit() {
@@ -22,6 +26,13 @@ export class ThermometerComponent implements OnInit {
 
   	ngOnDestroy() {
   		this._subscription.unsubscribe();
+  	}
+
+  	calcHeight() {
+  		/** 3.0 degrees is at 30, 100 per degree **/
+  		let tempOffset = 3.0-this.temperature;
+  		let pxOffset = tempOffset*100
+  		this.thermHeight = 30+pxOffset;
   	}
 
 
